0a94a8ba6934c7c8ee0fd44cb6e86d5001aeac97
bmoren <ben@benmoren.com>
2020-04-08 20:11:50 -0500

    fix ground in chrome & emoji as favicon

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
     for (let y = 0; y < window.innerHeight - (gridSpacing * 2); y += gridSpacing) {
 
       // let p = createP(randomEmoji()); // start with random emojis instead
-      let p = createP("♒︎");
+      let p = createP("〰");
 
       p.style('font-size', '2em')
       p.style('cursor', 'pointer'); //give the mario hand while mousing over
@@ -38,12 +38,38 @@ function randomEmoji() {
 
   let creature = ['🐛', '🐝', '🐞', '🐌', '🐜', '🕷', '🦗', '🐍', '🦎', '🐀', '🐇', '🦋', '🦔', '🕊']
 
+  let output;
+
   if (random(1) < 0.02) {
     let rand = floor(random(creature.length))
-    return creature[rand];
+    output = creature[rand];
   } else {
     let rand = floor(random(emojis.length))
-    return emojis[rand]
+    output =  emojis[rand]
   }
 
+
+  //favicon replacement
+  //https://css-tricks.com/emojis-as-favicons/
+  const linkForFavicon = document.querySelector(
+    `head > link[rel='icon']`
+  );
+
+  newFavicon = faviconTemplate`${output}`;
+    console.log(newFavicon);
+    linkForFavicon.setAttribute(`href`, `data:image/svg+xml,${newFavicon}`);
+
+  return output;
+
+}
+
+
+function faviconTemplate(string, icon) {
+  return `
+    <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+      <text y=%22.9em%22 font-size=%2290%22>
+        ${icon}
+      </text>
+    </svg>
+  `.trim();
 }
