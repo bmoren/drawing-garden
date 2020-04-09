582f16dce1becf2e20f7b0205270b3d4bffe875f
bmoren <ben@benmoren.com>
2020-04-09 17:54:14 -0500

    add sound, add social cards

--- sketch.js
+++ sketch.js
@@ -1,32 +1,101 @@
 let gridSpacing = 40
 
+let poly;
+let delay;
+let reverb;
+
+let notes = ["C","D","E","G"]
+// let notes = ["C","D","E","G","A"]
+
+let octave = 4
+let direction = 1
+
+let W = window.innerWidth
+let H = window.innerHeight
+
 
 function setup() {
 
+  var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-  for (let x = gridSpacing; x < window.innerWidth - gridSpacing; x += gridSpacing) {
-    for (let y = 0; y < window.innerHeight - (gridSpacing * 2); y += gridSpacing) {
+  console.log(isChrome, "chrome")
+
+  poly = new p5.PolySynth();
+
+  delay = new p5.Delay();
+  reverb = new p5.Reverb();
+
+
+  reverb.process(poly, 2, 2); //to only add reverb, toggle the others off.
+  // delay.process(poly, 0.50, 0.5, 2300);
+  // reverb.process(delay, 4, 2);
+
+// let i = 0;
+// let cts = ['c','l', 'i', 'c',' k', '〰', 'f','o','r', '〰', 's','o','u','n','d','!']
+
+
+for (let y = 0; y < H - (gridSpacing * 2); y += gridSpacing) {
+  for (let x = gridSpacing; x < W - gridSpacing; x += gridSpacing) {
+
+
+      let p
+
+      // if(i > cts.length-1){
+        p = createP("〰");
+      // }else{
+      //   p = createP(cts[i]);
+      //   p.class('sound-warning')
+      // }
+      // i++
 
       // let p = createP(randomEmoji()); // start with random emojis instead
-      let p = createP("〰");
-
       p.position(x, y)
-      p.mousePressed(pick)
+      p.mousePressed(changeEmoji)
       p.mouseOver(changeEmoji)
 
     }
   }
 }
 
+function mousePressed(){
+  userStartAudio()
+
+  // let sw = document.getElementsByClassName('sound-warning')
+  // console.log(sw)
+  // for (let element of sw) {
+  //   console.log(element);
+  //     element.innerText = "〰";
+  // }
+
+
+}
+
 function changeEmoji() {
+  userStartAudio()
+
+  let randNote = floor(random(notes.length))
+
+  if(random(1)<0.1){
+    octave+= direction
+  }
+  if(octave >= 6){
+    direction = -1
+  }
+  if(octave <= 4){
+    direction = 1
+  }
+
+  poly.play(notes[randNote] + octave , .15, 0, 0.25);
+
+
   //change the html inside of THIS p tag to be a new random emoji
   this.html(randomEmoji())
 }
 
-
-function pick(){
-  this.html("〰")
-}
+//
+// function pick(){
+//   this.html("〰")
+// }
 
 //utility function
 function randomEmoji() {
@@ -37,7 +106,7 @@ function randomEmoji() {
 
   let output;
 
-  if (random(1) < 0.02) {
+  if (random(1) < 0.03) {
     let rand = floor(random(creature.length))
     output = creature[rand];
   } else {
